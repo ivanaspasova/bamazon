bf691f89012c0e2027ea502dbaff89395f0011bd
ivanaspasova <ivannaspasov@gmail.com>
2019-02-14 20:22:35 -0600

    Upload pictures

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,7 @@
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-
-  port: 3306,
-
-  user: "root",
-
-  password: "",
-  database: "bamazon"
+var conn = require("./connection.js");
+var connection = conn.connection;
+conn.connectDB();
+connection.query("SELECT * FROM products", function(err, results, fields) {
+  if (err) throw err;
+  console.log(results);
 });
